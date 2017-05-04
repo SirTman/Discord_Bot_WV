51fb18ddafc9ba0dca6e62d8b1cbf1d850add425
SirTman <theo.inc.au@gmail.com>
2017-05-05 04:17:04 +0930

    Combat overhaul

--- Discordbot/Discordbot/Mybot.cs
+++ Discordbot/Discordbot/Mybot.cs
@@ -26,6 +26,7 @@ namespace Discordbot
 
     class Fighter
     {
+        
         public Fighter()
         {
             string F_Name = "";
@@ -59,14 +60,11 @@ namespace Discordbot
         }
     }
     */
-    
-
-
-
     class Mybot
     {
         DiscordClient discord;
         CommandService commands;
+        int RoundNUM = 0;
 
         public Mybot()
         {
@@ -76,25 +74,35 @@ namespace Discordbot
 
             discord = new DiscordClient(x =>
             {
+                x.AppName = "Minerva";
                 x.LogLevel = LogSeverity.Info;
                 x.LogHandler = Log;
             });
-            
+            discord.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity}] {e.Source}{ e.Message}");
+
+
             //Call Initialiser
             discord.UsingCommands(x => 
             {
                 x.PrefixChar = '+';
                 x.AllowMentionPrefix = true;
+                x.HelpMode = HelpMode.Public;
             });
             commands = discord.GetService<CommandService>();
 
             //Commands Classes go here
             Ping();
+            ClearMSG();
+
             RefisterDMGGenCommand();
             RefisterDodgeGenCommand();
 
+            //Fighter stuff
+            RefisterFighterdodge();
+            RefisterFighterattack();
 
-
+            
+            Round();
 
             //Stuff used to connect it to the server
             discord.ExecuteAndWait(async () =>
@@ -115,26 +123,152 @@ namespace Discordbot
                  await e.Channel.SendMessage("```Hiya~ I'm online.```");
             });
         }
-        
-        //Roll ATK
-        private void RefisterDMGGenCommand()
+    
+        private void ClearMSG()
+        {
+            commands.CreateCommand("ClearMSG")
+                .Do(async (e) =>
+                {
+                    Message[] msgtodelete;
+                    msgtodelete = await e.Channel.DownloadMessages(100);
+
+                    await e.Channel.DeleteMessages(msgtodelete);
+                });
+        }
+
+        //Roll ATK *NEW*
+        private void RefisterFighterattack()
         {
             commands.CreateCommand("ATK")
-                .Parameter("Who", ParameterType.Optional)
+                .Description("User [Base Power] Mod[Added Together]")
+                .Parameter("User", ParameterType.Required)
+                .Parameter("Basepower", ParameterType.Required)
+                .Parameter("Mod", ParameterType.Optional)
                 .Do(async (e) =>
                 {
                     Random rnd = new Random();
-                    var dmgvalue = rnd.Next(1, 50);
-                    string DMG = dmgvalue.ToString();
-                    await e.Channel.SendMessage(DMG + " " + e.GetArg("Who"));
+                    float BasePower;
+                    float Mod;
+                    if (Single.TryParse(e.GetArg("Basepower"), out BasePower))
+                    {
+                        bool Powerincrease = false;
+                        //Base Power of 50 or less
+                        if (BasePower <= 50)
+                        {
+                            BasePower += rnd.Next(31);
+                            Powerincrease = true;
+                        }
+                        //Base Power of 70 or less
+                        if (BasePower <= 70 && Powerincrease == false)
+                        {
+                            BasePower += rnd.Next(21);
+                            Powerincrease = true;
+                        }
+                        //Base Power of 70 or less
+                        if (BasePower <= 90 && Powerincrease == false)
+                        {
+                            BasePower += rnd.Next(11);
+                            Powerincrease = true;
+                        }
+                        //mod hanndler
+                        if (e.GetArg("Mod") != null)
+                        {
+                            if (Single.TryParse(e.GetArg("Mod"), out Mod))
+                            {
+                                BasePower *= Mod;
+                            }
+                            else
+                            {
+
+                            }
+                        }
+                        //Critical hit & Message exicution
+                        int ChanceOfCrit = rnd.Next(11);
+                        if(ChanceOfCrit == 10)
+                        {
+                            int crit = rnd.Next(2, 5);
+                            BasePower *= crit;
+                            string Crittype = "";
+
+                            //2=Nice, 3=Good, 4=Perfect
+                            if (crit == 2)
+                            {
+                                Crittype = "Nice";
+                            }
+                            else if (crit == 3)
+                            {
+                                Crittype = "Good";
+                            }
+                            else if (crit == 4)
+                            {
+                                Crittype = "Perfect";
+                            }
+                            await e.Channel.SendMessage("```" + "Critical Hit " + Crittype + "```");
+                        }
+                        string DMG = BasePower.ToString();
+                        await e.Channel.SendMessage("```" + e.GetArg("User") + " dealt " + DMG + "```");
+                    }
+                    //Failed
+                    else
+                    {
+                        await e.Channel.SendMessage("```" + e.GetArg("Basepower") + " Isn't vaild```");
+                    }
+                        
+
+                   
 
                 });
         }
-        
-        //Roll Dodge
-        private void RefisterDodgeGenCommand()
+
+        //Roll Dodge *NEW*
+        private void RefisterFighterdodge()
         {
             commands.CreateCommand("Dodge")
+                .Description("Foe User Min2Dodge")
+                .Parameter("Foe", ParameterType.Required)
+                .Parameter("User", ParameterType.Required)
+                .Parameter("Min", ParameterType.Optional)
+                .Do(async (e) =>
+                {
+                    Random rnd = new Random();
+                    int Min = 5;
+                    if (Int32.TryParse(e.GetArg("Min"), out Min)) { }
+                    int NUM = rnd.Next(11);
+                    string Rolled = NUM.ToString();
+                    if(NUM <= Min)
+                    {
+                        await e.Channel.SendMessage("```" + e.GetArg("Foe") + " failed to dodge " + e.GetArg("User") + "'s attack(" + Rolled + ")```");
+                    }
+                    else
+                    {
+                        await e.Channel.SendMessage("```" + e.GetArg("Foe") + " dodged " + e.GetArg("User") + "'s attack(" + Rolled + ")```");
+                    }
+                });
+        }
+
+        private void Round()
+        {
+            commands.CreateCommand("Round")
+                .Description("Keeps track of rounds")
+                .Do(async (e) =>
+                {
+                    RoundNUM += 1;
+                    String NUM = RoundNUM.ToString();
+                    await e.Channel.SendMessage("```ROUND: #" + NUM + "```");
+                });
+            commands.CreateCommand("CLRRound")
+               .Description("Clears the Round number")
+               .Do(async (e) =>
+               {
+                   RoundNUM = 0;
+                   await e.Channel.SendMessage("```Rounds reset```");
+               });
+        }
+        //------------------------------------------
+        //Roll Dodge*Old*
+        private void RefisterDodgeGenCommand()
+        {
+            commands.CreateCommand("DodgeOld")
                 .Parameter("Who", ParameterType.Optional)
                 .Do(async (e) =>
             {
@@ -155,7 +289,20 @@ namespace Discordbot
 
             });
         }
+        //Roll ATK*Old*
+        private void RefisterDMGGenCommand()
+        {
+            commands.CreateCommand("DMG")
+                .Parameter("Who", ParameterType.Optional)
+                .Do(async (e) =>
+                {
+                    Random rnd = new Random();
+                    var dmgvalue = rnd.Next(1, 50);
+                    string DMG = dmgvalue.ToString();
+                    await e.Channel.SendMessage(DMG + " " + e.GetArg("Who"));
 
+                });
+        }
 
         //End
         private void Log(object sender, LogMessageEventArgs e)
