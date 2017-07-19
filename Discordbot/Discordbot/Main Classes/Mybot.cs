4197a7d3700f7a7167f1c062f663d6598cee672e
SirTman <theo.inc.au@gmail.com>
2017-07-19 22:49:54 +0930

    Everything fixed

--- Discordbot/Discordbot/Main Classes/Mybot.cs
+++ Discordbot/Discordbot/Main Classes/Mybot.cs
@@ -29,12 +29,12 @@ namespace Discordbot
                 x.LogHandler = Log;
             });
             discord.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity}] {e.Source}{ e.Message}");
-
+            
 
             //Call Initialiser
             discord.UsingCommands(x => 
             {
-                x.PrefixChar = '+';
+                x.PrefixChar = '!';
                 x.AllowMentionPrefix = true;
                 x.HelpMode = HelpMode.Public;
             });
@@ -43,11 +43,12 @@ namespace Discordbot
             //Commands Classes go here
             Ping();
             ClearMSG();
-
+            
             //Fighter stuff
             RefisterFighterdodge();
             RefisterFighterattack();
-            VS();
+            Percent();
+            //VS();
             
 
 
@@ -57,6 +58,8 @@ namespace Discordbot
             discord.ExecuteAndWait(async () =>
             {
                 await discord.Connect("MzAyNTExOTY0OTcyNzc3NDgz.C9NOVA.intlnTR8dIUFEFgW9Y09iaoPS7Y", TokenType.Bot);
+                Game g = new Game("with something kinda lewd~", GameType.Default, "https://www.youtube.com/playlist?list=PLszcSON51lz1qLvyggPS0Xt51Y5XCIM5e");
+                discord.SetGame(g);
             });
             
 
@@ -74,6 +77,7 @@ namespace Discordbot
             commands.CreateCommand("Ping") .Do(async (e) =>
             {
                  await e.Channel.SendMessage("```Hiya~ I'm online.```");
+                
             });
         }
     
@@ -82,6 +86,7 @@ namespace Discordbot
             commands.CreateCommand("ClearMSG")
                 .Do(async (e) =>
                 {
+                    
                     Message[] msgtodelete;
                     msgtodelete = await e.Channel.DownloadMessages(100);
 
@@ -99,30 +104,11 @@ namespace Discordbot
                 .Parameter("Mod", ParameterType.Optional)
                 .Do(async (e) =>
                 {
-                    Random rnd = new Random();
+                    Random rnd = new Random((int)DateTime.Now.Ticks);
                     float BasePower;
                     float Mod;
                     if (Single.TryParse(e.GetArg("Basepower"), out BasePower))
                     {
-                        bool Powerincrease = false;
-                        //Base Power of 50 or less
-                        if (BasePower <= 50)
-                        {
-                            BasePower += rnd.Next(31);
-                            Powerincrease = true;
-                        }
-                        //Base Power of 70 or less
-                        if (BasePower <= 70 && Powerincrease == false)
-                        {
-                            BasePower += rnd.Next(21);
-                            Powerincrease = true;
-                        }
-                        //Base Power of 70 or less
-                        if (BasePower <= 90 && Powerincrease == false)
-                        {
-                            BasePower += rnd.Next(11);
-                            Powerincrease = true;
-                        }
                         //mod hanndler
                         if (e.GetArg("Mod") != null)
                         {
@@ -139,22 +125,34 @@ namespace Discordbot
                         int ChanceOfCrit = rnd.Next(11);
                         if(ChanceOfCrit == 10)
                         {
-                            int crit = rnd.Next(2, 5);
-                            BasePower *= crit;
+                            int crit = rnd.Next(2, 7);
+                            
                             string Crittype = "";
-
                             //2=Nice, 3=Good, 4=Perfect
                             if (crit == 2)
                             {
+                                BasePower *= 1.25f;
                                 Crittype = "Nice";
                             }
                             else if (crit == 3)
                             {
+                                BasePower *= 1.5f;
                                 Crittype = "Good";
                             }
                             else if (crit == 4)
                             {
+                                BasePower *= 1.5f;
+                                Crittype = "Excelent";
+                            }
+                            else if (crit == 5)
+                            {
+                                BasePower *= 1.75f;
                                 Crittype = "Perfect";
+                            }
+                            else if (crit == 6)
+                            {
+                                BasePower *= 2f;
+                                Crittype = "Row Row Fight the power";
                             }
                             await e.Channel.SendMessage("```" + "Critical Hit " + Crittype + "```");
                         }
@@ -183,21 +181,46 @@ namespace Discordbot
                 .Parameter("Min", ParameterType.Optional)
                 .Do(async (e) =>
                 {
-                    Random rnd = new Random();
+                    Random rnd = new Random((int)DateTime.Now.Ticks);
                     int Min = 5;
                     if (Int32.TryParse(e.GetArg("Min"), out Min)) { }
                     int NUM = rnd.Next(11);
                     string Rolled = NUM.ToString();
-                    if(NUM <= Min)
+                    if(NUM < Min)
                     {
-                        await e.Channel.SendMessage("```" + e.GetArg("Foe") + " failed to dodge " + e.GetArg("User") + "'s attack(" + Rolled + ")```");
+                        await e.Channel.SendMessage(CONT + e.GetArg("Foe") + " failed to dodge " + e.GetArg("User") + "'s attack(" + Rolled + ")```");
                     }
                     else
                     {
-                        await e.Channel.SendMessage("```" + e.GetArg("Foe") + " dodged " + e.GetArg("User") + "'s attack(" + Rolled + ")```");
+                        await e.Channel.SendMessage(CONT + e.GetArg("Foe") + " dodged " + e.GetArg("User") + "'s attack(" + Rolled + ")```");
                     }
                 });
         }
+        //Pesentage Roller *NEW*
+        private void Percent()
+        {
+            commands.CreateCommand("perc")
+                .Description("Chance Roller, X% Condition Target")
+                .Parameter("Chance", ParameterType.Required)
+                .Parameter("Condition", ParameterType.Required)
+                .Parameter("Target", ParameterType.Required)
+                .Do(async (e) =>
+                {
+                    Random rnd = new Random((int)DateTime.Now.Ticks);
+                    int Chance = 0;
+                    if (Int32.TryParse(e.GetArg("Chance"), out Chance)) { }
+                    int Roll = rnd.Next(1, 101);
+                    if (Roll <= Chance)
+                    {
+                        await e.Channel.SendMessage("`" + e.GetArg("Target") + " became " + e.GetArg("Condition") + "`");
+                    }
+                    else
+                    {
+                        await e.Channel.SendMessage("`Nothing Happend`");
+                    }
+                });
+        }
+
 
         private void Round()
         {
